b5e3fe9bfeb1130e2bf48ade021ee5bd732f2d2e
NicolyLana <nicolylana.dev@gmail.com>
2024-03-03 19:03:27 -0300

    ADD: Testes model candidato

--- test/models/candidato_test.rb
+++ test/models/candidato_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class CandidatoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Cadastrar um candidato com dados validos' do
+    candidato = Candidato.new nome: 'Ricardo Oliveira', email: 'ricardo@email.com',
+                              cpf: '96872396032', dataDeNascimento: '30/08/1999', telefone: '87958854774'
+    assert candidato.save
+  end
+
+  test 'Cadastrar um candidato com dados invalidos' do
+    candidato = Candidato.new nome: 'Ricardo Oliveira', email: 'ricardo@email.com',
+                              cpf: '', dataDeNascimento: '30/08/1999', telefone: '87958854774'
+    assert_not candidato.save
+  end
 end
