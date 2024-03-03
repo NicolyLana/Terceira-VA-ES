2bed4d7d24650a83c30b0e5b031acd2d53a6c6a1
NicolyLana <nicolylana.dev@gmail.com>
2024-03-03 19:21:52 -0300

    ADD: Testes model vaga

--- test/models/vaga_test.rb
+++ test/models/vaga_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class VagaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Cadastrar uma vaga com dados validos' do
+    empregador = Empregador.new nome: 'Marcia Cristina', nomeEmpresa: 'Alelo Produções', email: 'alelo@email.com',
+                                endereco: 'Rua acostamento, 657', telefone: '11948842332', cnpj: '51.489.289/0001-95'
+    assert empregador.save
+
+    vaga = Vaga.new nome: 'Dev pleno', descricao: 'Vaga para dev', salario: '8000', empregador_id: empregador.id
+
+    assert vaga.save
+  end
+
+  test 'Tentar editar uma vaga com dados invalidos' do
+    empregador = Empregador.new nome: 'Marcia Cristina', nomeEmpresa: 'Alelo Produções', email: 'alelo@email.com',
+                                endereco: 'Rua acostamento, 657', telefone: '11948842332', cnpj: '51.489.289/0001-95'
+    assert empregador.save
+
+    vaga = Vaga.new nome: 'Dev pleno', descricao: '', salario: 'aqui', empregador_id: empregador.id
+
+    assert_not vaga.save
+  end
 end
