c2c78491478fd58db577236c052174104a648340
NicolyLana <nicolylana.dev@gmail.com>
2024-03-04 02:15:13 -0300

    Adicionar script de compilação para Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,4 +1,5 @@
-rem Conteúdo do render-build.sh
+# Conteúdo do render-build.sh
 bundle install
+bundle lock --add-platform x86_64-linux
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
