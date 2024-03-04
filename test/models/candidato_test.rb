1c9f5302bf3cd08189516c3ed888c6f43f665203
NicolyLana <nicolylana.dev@gmail.com>
2024-03-04 01:28:14 -0300

    FIX: Correções actions

--- test/models/candidato_test.rb
+++ test/models/candidato_test.rb
@@ -1,15 +1,16 @@
 require "test_helper"
 
 class CandidatoTest < ActiveSupport::TestCase
-  test 'Cadastrar um candidato com dados validos' do
-    candidato = Candidato.new nome: 'Ricardo Oliveira', email: 'ricardo@email.com',
-                              cpf: '96872396032', dataDeNascimento: '30/08/1999', telefone: '87958854774'
+  test 'deve cadastrar um candidato com dados válidos' do
+    candidato = Candidato.new(nome: 'Ricardo Oliveira', email: 'ricardo@email.com',
+                              cpf: '96872396032', dataDeNascimento: '30/08/1999', telefone: '87958854774')
     assert candidato.save
   end
 
-  test 'Cadastrar um candidato com dados invalidos' do
-    candidato = Candidato.new nome: 'Ricardo Oliveira', email: 'ricardo@email.com',
-                              cpf: '', dataDeNascimento: '30/08/1999', telefone: '87958854774'
+  test 'não deve cadastrar um candidato com dados inválidos' do
+    candidato = Candidato.new(nome: 'Ricardo Oliveira', email: 'ricardo@email.com',
+                              cpf: '', dataDeNascimento: '30/08/1999', telefone: '8795885')
     assert_not candidato.save
   end
 end
+
--- test/models/empregador_test.rb
+++ test/models/empregador_test.rb
@@ -1,15 +1,17 @@
 require "test_helper"
 
 class EmpregadorTest < ActiveSupport::TestCase
-  test 'Cadastrar um empregador com dados validos' do
-    empregador = Empregador.new nome: 'Marcia Cristina', nomeEmpresa: 'Alelo Produções', email: 'alelo@email.com',
-                              endereco: 'Rua acostamento, 657', telefone: '11948842332', cnpj: '51.489.289/0001-95'
+  test 'deve cadastrar um empregador com dados válidos' do
+    empregador = Empregador.new(nome: 'Marcia Cristina', nomeEmpresa: 'Alelo Produções',
+                                email: 'alelo@email.com', endereco: 'Rua acostamento, 657',
+                                telefone: '11948842332', cnpj: '51.489.289/0001-95')
     assert empregador.save
   end
 
-  test 'Deletar um empregador com sucesso' do
-    empregador = Empregador.new nome: 'Ana Vitoria', nomeEmpresa: 'Eletro', email: 'eletro@email.com',
-                                endereco: 'Rua das flores, 1991', telefone: '11933335775', cnpj: '12.041.072/0001-66'
+  test 'deve deletar um empregador com sucesso' do
+    empregador = Empregador.new(nome: 'Ana Vitoria', nomeEmpresa: 'Eletro',
+                                email: 'eletro@email.com', endereco: 'Rua das flores, 1991',
+                                telefone: '11933335775', cnpj: '12.041.072/0001-66')
 
     assert empregador.save
     assert Empregador.exists?(empregador.id), 'Empregador não foi salvo corretamente no banco de dados'
@@ -18,10 +20,11 @@ class EmpregadorTest < ActiveSupport::TestCase
     assert_not Empregador.exists?(empregador.id), 'Empregador não foi removido corretamente do banco de dados'
   end
 
-  test 'Tentar cdastrar um empregador com dados invalidos' do
-    empregador = Empregador.new nome: 'Mar', nomeEmpresa: 'Alelo Produções', email: 'alelo@email.com',
-                                endereco: 'Rua acostamento, 657', telefone: '11948842332', cnpj: ''
+  test 'não deve cadastrar um empregador com dados inválidos' do
+    empregador = Empregador.new(nome: 'Mar', nomeEmpresa: 'Alelo Produções',
+                                email: 'alelo@email.com', endereco: 'Rua acostamento, 657',
+                                telefone: '11948842332', cnpj: '')
     assert_not empregador.save
   end
-
 end
+
