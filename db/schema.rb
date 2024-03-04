b7e4ac6ea4b357291756f37396b6e44c026937e9
NicolyLana <nicolylana.dev@gmail.com>
2024-03-04 02:26:30 -0300

    corrigindo script de compilação para Render

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_03_232915) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_03_154535) do
   create_table "candidatos", force: :cascade do |t|
     t.string "nome"
     t.string "email"
