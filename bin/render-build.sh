9a952336f2168baf7a67265c93e07468be180056
NicolyLana <nicolylana.dev@gmail.com>
2024-03-04 03:25:21 -0300

    corrigindo script de compilação para Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,4 @@
 # Conteúdo do render-build.sh
 bundle install
-bundle exec rails db:migrate RAILS_ENV=production
-bundle lock --add-platform x86_64-linux
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
